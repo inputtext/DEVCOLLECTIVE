ec6e42b8189c6e59a6bd36095241a4822fdeac75
Piyush Kanojiya <raat131221@gmail.com>
2026-07-31 23:26:50 +0530

    added the analytics

--- DevCollective/src/main.jsx
+++ DevCollective/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
